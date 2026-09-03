7909c5dd5ec912e9c17a9f24d18291749cd31716
InnoTekCR <innotekcr@gmail.com>
2026-09-03 23:58:27 +0000

    Fix Vite source alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,12 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/InnoTekCR/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
